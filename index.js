a50991f283a5802bc3274b52ac540b25b9d6b3f2
Himanshu <himanshushukla71.9@gmail.com>
2025-01-01 22:14:40 +0530

    Bug Fixed

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.post("/image-upload", upload.single("image"), async(req, res) => {
         if(!req.file){
             return res.status(400).json({error: true, message: "No images uploaded"})
         }
-        const imageUrl = `http://localhost:8000/uploads/${req.file.filename}`;
+        const imageUrl = `${process.env.SERVER_URL}/uploads/${req.file.filename}`;
 
         res.status(201).json({imageUrl});
     } catch(error){
